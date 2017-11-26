1defec0888d0475b56fbd7e9170c7ff850462eb0
Wes Reid <bwreid@gmail.com>
2017-11-25 17:21:08 -0800

    Add basic login route

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,17 @@
+const User = require('../models/User.model')
 const Controller = require('./Controller')('users')
 
-class UsersController extends Controller {}
+class UsersController extends Controller {
+  static login (req, res, next) {
+    const { email, password } = req.body
+    User.login(email, password)
+    .then(user => res.json({ user }))
+    .catch(() => {
+      const status = 401
+      const message = `Please check your email and password`
+      res.status(status).json({ status, message })
+    })
+  }
+}
 
 module.exports = UsersController
--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,16 @@
+const db = require('../db')
 const Model = require('./Model')('users')
 
-class User extends Model {}
+class User extends Model {
+  static login (email, password) {
+    return db('users').where({ email }).first().then(user => {
+      if (user.password === password) {
+        return user
+      } else {
+        throw new Error(`User authentication failed`)
+      }
+    })
+  }
+}
 
 module.exports = User
